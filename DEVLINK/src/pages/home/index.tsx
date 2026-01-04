ca42fc23d7a71fa9a54b5575810a5b1d4e61837f
fabricio <fabriciosantosc11@gmail.com>
2026-01-04 17:50:48 -0300

    linktree

---
+++ DEVLINK/src/pages/home/index.tsx
@@ -0,0 +1,8 @@
+
+export function Home(){
+    return (
+        <div className="w-full h-screen bg-neutral-900">
+            <h1 className="text-white text-3xl">Pagina Home</h1>
+        </div>
+    )
+}
---
+++ DEVLINK/src/pages/networks/index.tsx
@@ -0,0 +1,8 @@
+
+export function Networks(){
+    return (
+        <div>
+            <h1>Pagina Rede sociais</h1>
+        </div>
+    )
+}
