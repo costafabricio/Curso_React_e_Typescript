4d87326c0fd77a6ebef6b168d496d54979e46db4
fabricio <fabriciosantosc11@gmail.com>
2026-01-05 09:11:10 -0300

    criando componente de input e usando o firebase

--- DEVLINK/src/pages/login/index.tsx
+++ DEVLINK/src/pages/login/index.tsx
@@ -1,6 +1,35 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { Input } from "../../components/input";
+import { useState, type FormEvent } from "react";
+
+import {auth} from '../../services/firebaseConnection'
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 export function Login(){
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const navigate = useNavigate()
+
+
+    function handleSubmit(e: FormEvent){
+        e.preventDefault()
+
+        if(email === '' || password === ''){
+            alert('Preencha todos os campos')
+            return
+        }
+
+        signInWithEmailAndPassword(auth, email, password)
+        .then(() => {
+            console.log("LOGADO COM SUCESSO")
+            navigate("/admin", { replace:true })
+        }).catch((error) => {
+            console.log('Erro ao fazer login:')
+            console.log(error)
+        })
+    }
+
+
     return (
         <div className="flex w-full h-screen items-center justify-center flex-col">
            <Link to='/'>
@@ -8,6 +37,25 @@ export function Login(){
                  <span className="bg-gradient-to-r from-yellow-500 to-orange-400 bg-clip-text text-transparent">Links</span>
                 </h1>
            </Link>
+
+          <form onSubmit={handleSubmit} className="w-full max-w-xl flex flex-col px-2">
+             <Input placeholder="Digite seu e-mail..."
+                type="email"
+                value={email}
+                onChange={ (e) => setEmail(e.target.value)}
+             />
+
+              <Input placeholder="******"
+                type="password"
+                value={password}
+                onChange={ (e) => setPassword(e.target.value)}
+             />
+
+              <button type="submit"
+              className="h-9 bg-blue-600 rounded border-0 text-lg font-medium text-white">
+                    Acessar
+              </button>
+          </form>
         </div>
     )
 }
