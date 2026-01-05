f1558009f9a1c7cff9187a84ea6293e1ff184592
fabricio <fabriciosantosc11@gmail.com>
2026-01-04 21:28:54 -0300

    criando pagina home e pagina login

--- DEVLINK/src/pages/home/index.tsx
+++ DEVLINK/src/pages/home/index.tsx
@@ -1,8 +1,29 @@
+import { Social } from "../../components/social"
+
+import {FaFacebook, FaInstagram, FaYoutube, FaLinkedin, FaGithub} from 'react-icons/fa'
 
 export function Home(){
     return (
-        <div className="w-full h-screen bg-neutral-900">
-            <h1 className="text-white text-3xl">Pagina Home</h1>
+        <div className="flex flex-col w-full py-4 items-center justify-center">
+            <h1 className="md:text-4xl text-3xl font-bold text-white mt-20">Fabricio Costa</h1>
+            <span className="text-gray-50 mb-5 mt-3">Veja meus links 👇</span>
+
+            <main className="flex flex-col w-11/12 max-w-xl text-center">
+                <section className="bg-white mb-4 w-full py-2 rounded-lg select-none transition-transform hover:scale-105 cursor-pointer">
+                    <a>
+                        <p className="text-base md:text-lg">
+                            Canal do youtube
+                        </p>
+                    </a>
+                </section>
+
+                <footer className="flex justify-center gap-3 my-4">
+                    <Social url="https://www.linkedin.com/in/fabricio-costa-777430310/">
+                        <FaLinkedin size={35} color="#FFF"/>
+                    </Social>
+                </footer>
+
+            </main>
         </div>
     )
 }
