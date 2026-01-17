0b66d19dfad909950a670971a704a10ce284856a
fabricio <fabriciosantosc11@gmail.com>
2026-01-17 16:28:51 -0300

    estilizando a pagina home

--- 10_CARRINHO/src/pages/home/index.tsx
+++ 10_CARRINHO/src/pages/home/index.tsx
@@ -1,6 +1,31 @@
+import { BsCartPlus } from "react-icons/bs";
 
 export function Home() {
     return(
-        <h1>Pagina Home</h1>
+        <div>
+            <main className="w-full max-w-7xl px-4 mx-auto"> 
+              <h1 className="font-bold text-2xl mb-4 mt-10 text-center">Produtos em Alta</h1>
+
+              <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-5">
+                <section className="w-full">
+                    <img className="w-full rounded-lg max-h-70 mb-2"/>
+
+                    <p className="font-medium mt-1 mb-2">Fone de ouvido</p>
+
+                    <div className="flex gap-3 items-center">
+                        <strong className="text-zinc-700/90">
+                            R$ 800
+                        </strong>
+                        <button className="bg-zinc-900 p-1 rounded">
+                            <BsCartPlus size={20} color="#FFF"/>
+                        </button>
+                    </div>
+                    
+                </section>
+
+              </div>
+
+            </main>
+        </div>
     )
 }
