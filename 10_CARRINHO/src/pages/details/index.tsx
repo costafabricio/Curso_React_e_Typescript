426c19ee60562a1642446ff6757acf44b22afc3d
fabricio <fabriciosantosc11@gmail.com>
2026-02-04 17:13:44 -0300

    fiz o desafio do projeto, e finalizei o projeto

--- 10_CARRINHO/src/pages/details/index.tsx
+++ 10_CARRINHO/src/pages/details/index.tsx
@@ -1,44 +1,70 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState, useContext } from "react"
 import { api } from "../../services/api"
-import { useParams } from "react-router-dom"
+import { useParams, useNavigate } from "react-router-dom"
+import { Link } from "react-router-dom";
 
-interface DetailsProps {
-    id: number,
-    title: string,
-    description: string,
-    price: number,
-    cover: string
-}
+import { BsCartPlus } from "react-icons/bs";
+import { CartContext } from "../../contexts/CartContexts";
+import toast from "react-hot-toast";
+import { type ProductProps } from "../home";
 
 export function Details() {
     const {id} = useParams()
-    const [details, setDetails] = useState<DetailsProps | null>(null) 
+    const [product, setProduct] = useState<ProductProps>() 
+    const {addItemCart} = useContext(CartContext)
+    const navigate = useNavigate()
 
     useEffect(() => {
         async function getDetails() {
             const response = await api.get(`/products/${id}`)
-            setDetails(response.data)
+            setProduct(response.data)
             console.log(response.data)
         }
         getDetails()
-    },[])
+    },[id])
+
+     function handleAddCartItem(product: ProductProps){
+        toast.success("Produto adicionado no carrinho")
+
+       addItemCart(product)
+       navigate("/cart")
+    }
 
     return(
-      <div className="w-full">
-        <section key={details?.id} className="flex flex-row">
-            <img className="h-80"  
-            src={details?.cover} 
-            alt={details?.title} />
+        <div>
+          <main className="w-full max-w-7xl px-4 mx-auto my-6">
 
-            <div >
-            <h1 className="mb-2 mt-4 font-bold text-2xl">{details?.title}</h1>
-            <p className="mb-2">{details?.description}</p>
-            <p>{details?.price.toLocaleString("pt-BR", {
-                style: "currency",
-                currency: "BRL"
-            })}</p> 
-            </div>
-        </section>
-     </div>
-    )
+            {product && (
+             <section className="w-full">
+
+                <div className="flex flex-col lg:flex-row">
+
+                   <img className="flex-1 w-full max-h-72 object-contain"  
+                   src={product?.cover} 
+                   alt={product?.title}/>
+            
+                  <div className="flex-1">
+                      <h1 
+                        className="mb-2 mt-4 font-bold text-2xl">{product?.title}
+                      </h1>
+                       <p className="mb-2">{product?.description}</p>
+                      <strong className="text-zinc-700/90 text-xl">
+                        {product?.price.toLocaleString("pt-BR", {
+                        style: "currency",
+                        currency: "BRL"})}
+                      </strong> 
+
+                      <button className="bg-zinc-900 p-1 rounded ml-3" 
+                        onClick={() => handleAddCartItem(product)}>
+                        <BsCartPlus size={20} color="#FFF"/>
+                      </button>
+
+                  </div>
+
+              </div>
+          </section>
+         )}
+    </main>
+  </div>
+  )
 }
